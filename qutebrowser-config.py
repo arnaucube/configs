d16296b545941667c0d589389685e56b42b4c6f7
arnaucube <root@arnaucube.com>
2024-07-11 19:03:05 +0200

    add qutebrowser config file, update raspberrypi script

---
+++ qutebrowser-config.py
@@ -0,0 +1,5 @@
+# this file has to be placed (and renamed) at: ~/.config/qutebrowser/config.py
+
+config.load_autoconfig()
+
+config.set("colors.webpage.darkmode.enabled", True)
--- raspberry/install-raspberry-minimum.sh
+++ raspberry/install-raspberry-minimum.sh
@@ -12,8 +12,8 @@ echo "installing tmux"
 sudo apt-get -y install tmux
 
 echo "setting nvim config"
-mkdir ~/config
-mkdir ~/config/nvim
+mkdir ~/.config
+mkdir ~/.config/nvim
 cp ../init.vim ~/.config/nvim/init.vim
 
 echo "setting xmodmap config"
@@ -33,3 +33,16 @@ cat ../.bashrc >> ~/.bashrc
 # static ip_address=192.168.1.150/24
 # static routers=192.168.1.1
 # static domain_name_servers=192.168.1.1
+
+# UPDATE: in newer systems (ie. raspbian on rpi5):
+# vim /etc/network/interfaces:
+#
+# auto eth0
+# iface eth0 inet static
+#     address 192.168.1.150
+#     netmask 255.255.255.0
+#     gateway 192.168.1.1
+#     dns-nameservers 192.168.1.1
+#
+# and then restart the network interfaces with:
+# sudo systemctl restart networking
