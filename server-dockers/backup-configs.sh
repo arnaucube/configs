88f116f1cb7616719f9a0d02e867969f56753220
arnaucube <git@arnaucube.com>
2026-02-08 18:08:54 +0100

    add parse-blueray sh script, and backup server-dockers config

--- server-dockers/backup-configs.sh
+++ server-dockers/backup-configs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SERVER=$1
-PASSW=$2
+PASSW=$2 # the password that appears at the configs to be replaced by $NEW_PASSW
 NEW_PASSW="YOURPASSWORDHERE"
 
 if [ "$#" -ne 2 ]; then
@@ -9,7 +9,7 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-rsync -avz --progress --exclude={'*.log','emby','jellyfin','qbittorrent','slskd','wallabag','prowlarr','sonarr','radarr','bazarr'} $SERVER:~/server-dockers/docker-compose.yml $SERVER:/mnt/usb/configurations ./
+rsync -avz --progress --exclude={'*.log','emby','jellyfin','qbittorrent','slskd','wallabag','prowlarr','sonarr','radarr','bazarr','qui'} $SERVER:~/server-dockers/docker-compose.yml $SERVER:/mnt/usb/configurations ./
 
 # replace PASSW by NEW_PASSW (also in subdirs)
 find . -type f -exec \
